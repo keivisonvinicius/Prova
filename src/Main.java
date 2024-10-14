9e113d968b3e6cbf0042f42ab224dcb2515cb4b3
Keivison Vinicius Da Silva Ferreira <Keivison@academicos.com>
2024-10-14 20:08:47 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("1984", "George Orwell", 1949);
+        Livro livro2 = new Livro("Dom Quixote", "Miguel de Cervantes", 1605);
+        Usuario usuario1 = new Usuario("Ana");
+        Biblioteca biblioteca = new Biblioteca("Biblioteca Central");
+        biblioteca.adicionarLivro(livro1);
+        biblioteca.adicionarLivro(livro2);
+        biblioteca.exibirLivrosDisponiveis();
+        usuario1.emprestarLivro(biblioteca, livro1);
+        usuario1.exibirLivrosEmprestados();
+        biblioteca.exibirLivrosDisponiveis();
+        usuario1.devolverLivro(biblioteca, livro1);
+    }
+}
